ebef93a82b707e367362d0a92beaac64afad5b9b
Leonir Deolindo <leonir.ad@gmail.com>
2020-09-28 07:30:19 -0300

    Initial Commit

---
+++ ProjectNoName/ProjectNoNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectNoNameApp.swift
+//  ProjectNoName
+//
+//  Created by Leonir Alves Deolindo on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ProjectNoNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
